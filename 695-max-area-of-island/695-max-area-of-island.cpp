a34248e35988787e86f81912e20d6f2fc614a2f1
Mina Samy <mina.yousry.iti@gmail.com>
2022-07-12 16:49:54 +0200

    Time: 17 ms (93.22%), Space: 23.1 MB (89.31%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,96 +1,29 @@
 class Solution {
 public:
-    
-    int BFS(vector<vector<int>>& grid,pair<int,int> element,vector<vector<bool>> &visited)
-    {
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
         
-        int no_of_connections = 0;
+        int result = 0;
         
-        pair<int,int> front_element;
-        queue<pair<int,int>> elements;
-        
-        char directions_horizontal[2] = {1,-1};
-        char directions_vertical[2]   = {1,-1};
-        
-        elements.push(element);
-        visited[element.first][element.second] = true;
-        no_of_connections++;
-        
-        while(!elements.empty())
-        {
-           front_element = elements.front();
-           elements.pop();
-            
-            int new_index = 0;
-            
-            for(int v:directions_vertical)
-            {
-                new_index = front_element.first+v;
-                
-                if( ((new_index >= 0) && (new_index < m)) &&
-                    (grid[new_index][front_element.second] == 1) &&
-                    (visited[new_index][front_element.second] == false)
-                  )
-                {
-                    elements.push(make_pair(new_index,front_element.second));
-                    visited[new_index][front_element.second] = true;
-                    no_of_connections++;
-                }
-            }
-            
-            for(int h:directions_horizontal)
-            {
-                new_index = front_element.second+h;
-                
-                if( ((new_index >= 0) && (new_index < n)) &&
-                    (grid[front_element.first][new_index] == 1) &&
-                    (visited[front_element.first][new_index] == false)
-                  )
-                {
-                    elements.push(make_pair(front_element.first,new_index));
-                    visited[front_element.first][new_index] = true;
-                    no_of_connections++;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    result = max(result, dfs(grid, i, j, m, n));
                 }
             }
         }
         
-        return no_of_connections;
-        
-        
-        
+        return result;
     }
-    
-    int maxAreaOfIsland(vector<vector<int>>& grid)
-    {
-        
-        int m = grid.size();
-        int n = grid[0].size();
-        
-        int max_connections = 0;
-        int no_of_connections = 0;
-        
-        vector<vector<bool>> visited(m,vector<bool> (n, false)); 
-        
-        for(int i=0;i<m;++i)
-        {
-            for(int j=0;j<n;++j)
-            {
-                if((grid[i][j] == 1) && (visited[i][j] == false))
-                {
-                    pair<int,int> element = make_pair(i,j);
-                    no_of_connections = BFS(grid,element,visited);
-                    
-                    if(no_of_connections > max_connections)
-                    {
-                        max_connections = no_of_connections;
-                    }
-                }
-                
-            }
+private:
+    int dfs(vector<vector<int>>& grid, int i, int j, int m, int n) {
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == 0) {
+            return 0;
         }
+        grid[i][j] = 0;
         
-        return max_connections;
+        return 1 + dfs(grid, i - 1, j, m, n) + dfs(grid, i + 1, j, m, n)
+            + dfs(grid, i, j - 1, m, n) + dfs(grid, i, j + 1, m, n);
     }
 };
