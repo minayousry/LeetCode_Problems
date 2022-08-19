5521ba3076eb3cfa14a3abdd5e65aee26e72d904
Mina Samy <mina.yousry.iti@gmail.com>
2022-08-19 18:28:21 +0200

    Time: 475 ms (5.01%), Space: 71.4 MB (5.05%) - LeetHub

---
+++ 633-sum-of-square-numbers/633-sum-of-square-numbers.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c)
+    {
+        
+        bool is_square_sum = false;
+        unordered_map<int,bool> dict;
+        long int square_num = 0;
+        
+         if(c < 3)
+            return true;
+        
+        dict[0] = true; 
+        for(int i=0;i<c;++i)
+        {
+             square_num = pow(i,2);
+            
+             if(square_num <= static_cast<long int>(c))
+             {
+                if((square_num == (c - square_num)) || (dict.find(c - square_num) != dict.end()))
+                {
+                    is_square_sum = true;
+                    break;
+                }
+                dict[square_num] = true;    
+             }
+             else
+             {
+                 break;
+             }         
+        }
+   
+        return is_square_sum;
+    }
+
+};
