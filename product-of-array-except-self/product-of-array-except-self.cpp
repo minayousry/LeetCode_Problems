dba85c05f21255b058815e3b2fa9fd3358a5a07a
Mina Samy <mina.yousry.iti@gmail.com>
2023-04-27 02:08:57 +0200

    Time: 20 ms (89.98%) | Memory: 23.9 MB (90.08%) - LeetSync

--- product-of-array-except-self/product-of-array-except-self.cpp
+++ product-of-array-except-self/product-of-array-except-self.cpp
@@ -2,35 +2,22 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums)
     {
-        // Count the number of zeros in nums
-        int prefix[nums.size()];
-        int postfix[nums.size()];
-        vector<int> answer(nums.size(),0);
+        vector<int> answer(nums.size());
 
-        prefix[0] = 1;
+        answer[0] = 1;
         for(int i=1;i<nums.size();++i)
         {
-            prefix[i] = prefix[i - 1] *nums[i - 1];
+            answer[i] = answer[i - 1] *nums[i - 1];
         }
 
+        int suffix = 1;
 
-        postfix[nums.size() - 1] = 1;
-
-        for(int i=nums.size() - 2;i>=0;--i)
+        for(int i=nums.size() -1;i>=0;--i)
         {
-            postfix[i] = postfix[i + 1] *nums[i + 1];
+            answer[i] = answer[i] *suffix;
+            suffix *= nums[i];
         }
 
-        for(int i=0;i<nums.size();++i)
-        {
-            answer[i] = prefix[i]*postfix[i];
-        }
-
-
-
-
-
-    
         return answer;
     }
 };
