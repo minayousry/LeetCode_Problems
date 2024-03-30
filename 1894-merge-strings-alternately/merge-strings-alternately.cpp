feaeba7be3ae68f519ed015211c75f4e4319098f
Mina Samy <mina.yousry.iti@gmail.com>
2024-03-30 21:55:37 +0000

    Time: 0 ms (100.00%) | Memory: 7.7 MB (46.83%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -4,26 +4,35 @@ public:
     {
         int w1_size = word1.size();
         int w2_size = word2.size();
-
+        int i = 0;
         string total = "";
 
-        int i = 0;
-        int j = 0;
+        int min_size = std::min(w1_size,w2_size);
         
-        while((i < w1_size) || (j < w2_size))
+        while(i < min_size)
         {
             if(i < w1_size)
             {
                 total += word1[i];
-                i++;
             }
 
-            if(j < w2_size)
+            if(i < w2_size)
             {
-                total += word2[j];
-                j++;
+                total += word2[i];
             }
+            i++;
         }
+
+        if(min_size == w2_size)
+        {
+            total += word1.substr(min_size);
+        }
+        else
+        {
+            total += word2.substr(min_size);
+        }
+
+
 
         return total;
         
