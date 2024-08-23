abdd9f0a72d9b1bab0460277b64b8785916f5624
Mina Samy <mina.yousry.iti@gmail.com>
2024-08-23 22:46:42 +0100

    Time: 122 ms (80.74%), Space: 82.3 MB (65.72%) - LeetHub

---
+++ 0883-car-fleet/0883-car-fleet.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+
+        int fleet_destination;
+
+        vector<pair<int,int>> cars_info;
+        stack<double> time_to_dist;
+
+        for(int i=0;i<position.size();++i)
+        {
+            cars_info.push_back({position[i],speed[i]});
+        }
+
+        sort(cars_info.begin(),cars_info.end());
+
+        double time;
+
+        
+        for(int i=cars_info.size() - 1;i>=0;--i)
+        {
+            time = static_cast<double>(target - cars_info[i].first)/cars_info[i].second;
+
+            if(time_to_dist.empty() || (time > time_to_dist.top()))
+            {
+                time_to_dist.push(time);
+            }
+            
+        }
+        return time_to_dist.size();
+        
+    }
+};
