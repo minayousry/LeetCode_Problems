d224bf07d6bf79286400b8e9ae20baa625fe5c59
Mina Samy <mina.yousry.iti@gmail.com>
2024-08-24 22:20:32 +0100

    Time: 50 ms (6.06%), Space: 21.8 MB (21.98%) - LeetHub

---
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+
+        int max_pile = static_cast<int>(*(std::max_element(piles.begin(),piles.end())));
+
+        unsigned long int sum = 0;
+
+        int rate;
+
+        int start = 1;
+        int end = max_pile;
+
+        int result = max_pile;
+
+        while(start<=end)
+        {
+            rate = start + (end - start)/2;
+            
+            sum = 0; 
+            for(auto &pile:piles)
+            {
+                sum += ceil(static_cast<double>(pile)/rate);
+
+            }
+
+            if(sum <= h)
+            {
+                result = rate;
+                end = rate - 1;
+            }
+            else
+            {
+                start = rate + 1;
+            }
+
+        }
+
+        return result;
+
+        
+    }
+};
