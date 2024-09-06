eaf39a8a7297111f8b65656f352bc1aa2f755a49
Mina Samy <mina.yousry.iti@gmail.com>
2024-09-06 22:20:38 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        
+        int l,r;
+        l = 0;
+        r = 0;
+        int max_len = 0;
+        unordered_map<char,int> dict;
+
+        if(s.size() <= 1)
+        {
+            return s.size();
+        }
+
+        while(r < s.size())
+        {
+            
+            while(dict.find(s[r]) != dict.end())
+            {
+                dict.erase(s[l]);
+                l++;
+                max_len = max(max_len, (r - l + 1));
+            }
+            dict[s[r]] = 1;
+            r++;
+        }
+        return max_len;
+        
+    }
+};
