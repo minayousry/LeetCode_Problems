bfce38e572874b9a99a707291e089f515df799cc
Mina Samy <mina.yousry.iti@gmail.com>
2024-09-15 20:17:46 +0100

    Time: 0 ms (100%), Space: 7.3 MB (82.2%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.cpp
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -2,28 +2,24 @@ class Solution {
 public:
     bool isPerfectSquare(int num)
     {
-        bool status = false;
-        
-         if(num == 1)
-             return true;
-         else if(num < 4)
+        if (num == 1)
+            return true;
+        else if (num < 4)
             return false;
-        
+
         int start = 1;
-        int end = num/2;
-        int mid;
+        int end = num;
 
-        while(start <= end)
+        while (start <= end)
         {
-            mid = start + (end - start)/2; //1
+            int mid = start + (end - start) / 2;
 
-
-            if(((float)mid - ((float)num/(float)mid)) == 0.0)
+            // To avoid overflow, compare using division
+            if (mid == num / mid && num % mid == 0)
             {
-                 status = true;
-                 break;
+                return true;
             }
-            else if(mid < num/mid)
+            else if (mid < num / mid)
             {
                 start = mid + 1;
             }
@@ -31,10 +27,8 @@ public:
             {
                 end = mid - 1;
             }
-                
         }
 
-        return status;
+        return false;
     }
-        
 };
