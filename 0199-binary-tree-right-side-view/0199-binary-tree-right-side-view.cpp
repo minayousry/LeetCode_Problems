130e921193e609512499b287f84fc248f5d7c42e
Mina Samy <mina.yousry.iti@gmail.com>
2025-01-05 03:18:00 +0000

    Time: 0 ms (100%), Space: 15.2 MB (17.45%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -9,51 +9,44 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
-
         vector<int> result;
         queue<TreeNode*> data;
+        TreeNode* rightmost = nullptr;
 
-        if(root)
+        if(root != nullptr)
         {
             data.push(root);
-            result.push_back(root->val);
-
-            while(!data.empty())
-            {
-                int len = data.size();
-                stack<int> level_values;
-                for(int i=0;i<len;++i)
-                {
-                    auto elm = data.front();
-                    data.pop();
-
-                    if(elm->left)
-                    {
-                        data.push(elm->left);
-                        level_values.push(elm->left->val);
-                    }
-
-                    if(elm->right)
-                    {
-                        data.push(elm->right);
-                        level_values.push(elm->right->val);
-                    }
-                }
-                
-                if(level_values.size() > 0)
-                {
-                    result.push_back(level_values.top());
-                }
-                
-            }
         }
         
+        while(!data.empty())
+        {
+            int queue_size = data.size(); 
+            for(int i=0;i<queue_size;++i)
+            {
+                TreeNode* node = data.front();
+                data.pop();
 
+                rightmost = node;
+                
+                if(node->left)
+                {
+                    data.push(node->left);
+                }
+
+                if(node->right)
+                {
+                    data.push(node->right);
+                }
+            }
+            result.push_back(rightmost->val);
+            
+            
+            
+        }
         return result;
-        
+
     }
 };
