38dc1fb3cfbbeae1a8d9625eade988e77f9dfebe
Mina Samy <mina.yousry.iti@gmail.com>
2025-02-12 01:14:35 +0000

    Time: 0 ms (100%), Space: 23.1 MB (24.08%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -12,28 +12,33 @@
 class Solution {
 public:
 
-    pair<bool,int> dfs(TreeNode *root)
+    pair<bool,int>  getMaxHeight(TreeNode* root)
     {
-        pair<bool,int> res;
+        pair<bool,int> result;
 
         if(root == nullptr)
         {
-            res.first = true;
-            res.second = 0;
-            
-            return res;
+            result.first = true;
+            result.second = 0;
+            return result;    
         }
-        pair<bool,int> left = dfs(root->left);
-        pair<bool,int> right = dfs(root->right);
 
-
-        res.first = left.first && right.first && (abs(left.second - right.second) < 2);
-        res.second = 1 + max(left.second,right.second);
-        return res; 
+        pair<bool,int> lhs = getMaxHeight(root->left);
+        pair<bool,int> rhs = getMaxHeight(root->right);
         
+        bool balanced = lhs.first && rhs.first && (abs(lhs.second - rhs.second) <= 1U);
+        int height = 1 + max(lhs.second,rhs.second);
+
+        result.first = balanced;
+        result.second = height;
+
+        return result;         
+
     }
 
     bool isBalanced(TreeNode* root) {
-        return dfs(root).first;
+
+        return getMaxHeight(root).first;
+        
     }
 };
