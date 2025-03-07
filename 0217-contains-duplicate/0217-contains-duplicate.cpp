074e8802f8c91f0ac6e5e45392409bab0fef6d94
Mina Samy <mina.yousry.iti@gmail.com>
2025-03-07 21:00:43 +0000

    Time: 18 ms (92.45%), Space: 61.2 MB (88.81%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
-    bool containsDuplicate(vector<int>& nums) 
-    {
-        bool result = false;
+    bool containsDuplicate(vector<int>& nums) {
+        
+        if (nums.size() <= 1)
+        {
+            return false;
+        }
+
         sort(nums.begin(),nums.end());
 
-        for(int i=0;i<nums.size() - 1;++i)
+        for(int i=1;i<nums.size();++i)
         {
-            if(nums[i] == nums[i+1])
+            if(nums[i] == nums[i - 1])
             {
-                result = true;
-                break;
+                return true;
             }
         }
-        return result;
-        
+
+        return false;
     }
 };
