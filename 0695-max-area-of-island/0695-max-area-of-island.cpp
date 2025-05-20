7b0df145998bd68759660a2f269a760c9d088405
Mina Samy <mina.yousry.iti@gmail.com>
2025-05-20 22:28:09 +0100

    Time: 16 ms (12.74%), Space: 34.7 MB (7.67%) - LeetHub

---
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+
+    int isIsland(vector<vector<int>>& grid,vector<vector<bool>> &visited,int row,int col)
+    {
+        int max_islands = 1;
+        queue<pair<int,int>> islands;
+        vector<vector<int>> directions = {{1,0,},{-1,0},{0,1},{0,-1}};
+
+        islands.push(make_pair(row,col));
+
+        while(!islands.empty())
+        {
+            pair<int,int> island = islands.front();
+            visited[row][col] = true;
+            islands.pop();    
+
+            for(int i=0;i<4;i++)
+            {
+                int new_row = island.first + directions[i][0];
+                int new_col = island.second + directions[i][1];
+                
+                if(new_row >=0 && new_row < grid.size() && new_col >= 0 && new_col < grid[0].size() && 
+                    (visited[new_row][new_col] == false) && grid[new_row][new_col] == 1)
+                {
+                    islands.push(make_pair(new_row,new_col));
+                    visited[new_row][new_col] = true;
+                    max_islands++;
+                }
+                
+            }
+
+        }
+        return max_islands;
+    }
+
+
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int islands = 0;
+
+        int max_no = 0;
+
+        vector<vector<bool>> visited(grid.size(),vector<bool>(grid[0].size(),false));
+
+        for(int i=0;i<grid.size();++i)
+        {
+            for(int j=0;j<grid[0].size();++j)
+            {
+                if (grid[i][j] == 1 && !visited[i][j]) {
+                    islands = isIsland(grid, visited, i, j); 
+                    max_no = max(max_no,islands);
+                    
+                }
+            
+            }
+        }
+
+        return max_no; 
+    }
+};
