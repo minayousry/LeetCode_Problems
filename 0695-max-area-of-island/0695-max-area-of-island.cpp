2bdf8b3712a00be1a917d589ac782e9701e7f412
Mina Samy <mina.yousry.iti@gmail.com>
2025-05-20 22:32:11 +0100

    Time: 9 ms (19.52%), Space: 31.8 MB (19.43%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<vector<int>> directions = {{1,0,},{-1,0},{0,1},{0,-1}};
 
-    int getMaxIslands(vector<vector<int>>& grid,vector<vector<bool>> &visited,int row,int col)
+    int getMaxIslands(vector<vector<int>>& grid,int row,int col)
     {
         int max_islands = 1;
         queue<pair<int,int>> islands;
@@ -13,7 +13,8 @@ public:
         while(!islands.empty())
         {
             pair<int,int> island = islands.front();
-            visited[row][col] = true;
+
+            grid[row][col] = 0;
             islands.pop();    
 
             for(int i=0;i<4;i++)
@@ -21,11 +22,10 @@ public:
                 int new_row = island.first + directions[i][0];
                 int new_col = island.second + directions[i][1];
                 
-                if(new_row >=0 && new_row < grid.size() && new_col >= 0 && new_col < grid[0].size() && 
-                    (visited[new_row][new_col] == false) && grid[new_row][new_col] == 1)
+                if(new_row >=0 && new_row < grid.size() && new_col >= 0 && new_col < grid[0].size() && grid[new_row][new_col] == 1)
                 {
                     islands.push(make_pair(new_row,new_col));
-                    visited[new_row][new_col] = true;
+                    grid[new_row][new_col] = 0;
                     max_islands++;
                 }
                 
@@ -41,14 +41,12 @@ public:
 
         int max_no = 0;
 
-        vector<vector<bool>> visited(grid.size(),vector<bool>(grid[0].size(),false));
-
         for(int i=0;i<grid.size();++i)
         {
             for(int j=0;j<grid[0].size();++j)
             {
-                if (grid[i][j] == 1 && !visited[i][j]) {
-                    islands = getMaxIslands(grid, visited, i, j); 
+                if (grid[i][j] == 1) {
+                    islands = getMaxIslands(grid, i, j); 
                     max_no = max(max_no,islands);
                 }
             
