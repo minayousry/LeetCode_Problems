04f3b0f2bb87e26122704ed329d77048130ca4b7
Mina Samy <mina.yousry.iti@gmail.com>
2025-05-21 19:46:21 +0100

    Time: 44 ms (7.4%), Space: 9.9 MB (58.24%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,69 +1,40 @@
+#include<cctype>
+
 class Solution {
 public:
-    bool isPalindrome(string s)
-    {
-         
-        bool status = true;
-
+    bool isPalindrome(string s) {
+        bool result = true;
         
         if(s.size() <= 1)
         {
-            status = true;
+            return s.size();
         }
-        else
-        {
-            unsigned char data[255] = {0};
-        
-            for(unsigned char i = '0';i<='9';++i)
-            {
-                data[i] = 1;
-            }
-        
-        
-            for(unsigned char i = 'a';i<='z';++i)
-            {
-                data[i] = 1;
-            }
-        
-            for(unsigned char i = 'A';i<='Z';++i)
-            {
-                data[i] = 1;
-            }
-            
-            int l = 0;
-            int r = s.size() - 1;
-        
-            while(l < r)
-            {
-                while((l < r) && (data[s[l]] == 0u))
-                {
-                    l++;
-                }
-                
-                while((l < r) && (data[s[r]] == 0u))
-                {
-                    r--;
-                }
-                
-                if(s[l] > 90)
-                {
-                    s[l] -= 32;
-                }
-                
-                if(s[r] > 90)
-                {
-                    s[r] -= 32;
-                }
 
-                if(s[r] != s[l])
-                {
-                    status = false;
-                    break;
-                }
+        int l = 0;
+        int r = s.size() - 1;
+
+        while(l < r)
+        {
+            cout<<l;
+            while(l < r && !isalnum(s[l]))
+            {
                 l++;
+            }
+
+            while(l < r && !isalnum(s[r]))
+            {
                 r--;
             }
+
+            if(std::tolower(s[l]) != std::tolower(s[r]))
+            {
+                result = false;
+                break;
+            }
+            l++;
+            r--;
         }
-        return status;
+
+        return result;
     }
 };
