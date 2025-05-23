860ba1e56ec52b9032d733280ca95a1c565571cb
Mina Samy <mina.yousry.iti@gmail.com>
2025-05-23 21:03:04 +0100

    Time: 0 ms (100%), Space: 65.1 MB (68.72%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
-    int findDuplicate(vector<int>& nums)
-    {
-        unordered_set<int> data;
-        int set_len = data.size();
 
-        for(auto &num:nums)
+    
+    int findDuplicate(vector<int>& nums) {
+        
+        int slow_ind = 0;
+        int fast_ind = 0;
+
+        //find first intersection
+        do
         {
-            data.insert(num);
-            if(data.size() == set_len)
-            {
-                return num;
-            }
-            else
-            {
-                set_len = data.size();
-                
-            }
+            slow_ind = nums[slow_ind];
+            fast_ind = nums[nums[fast_ind]];
+        }while(slow_ind != fast_ind);
+
+
+        int slow2_ind = 0;
+
+        while(slow2_ind != slow_ind)
+        {
+            slow2_ind = nums[slow2_ind];
+            slow_ind = nums[slow_ind];
         }
-        return nums[0];
+
+        return slow_ind;
+
+
+        return 0;
     }
 };
