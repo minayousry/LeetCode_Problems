fb2419d079413f95cc92121f1c9f3c42c6dc31b2
Mina Samy <mina.yousry.iti@gmail.com>
2025-05-24 22:58:50 +0100

    Time: 0 ms (100%), Space: 9.8 MB (77.07%) - LeetHub

--- 1127-last-stone-weight/1127-last-stone-weight.cpp
+++ 1127-last-stone-weight/1127-last-stone-weight.cpp
@@ -1,31 +1,19 @@
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
-        priority_queue<int> pack;
-        int result = -1;
 
-        for(const auto& stone:stones)
+        priority_queue<int> weights(stones.begin(),stones.end());
+
+        while(weights.size() > 1)
         {
-            pack.push(stone);
+            int weight1 = weights.top();
+            weights.pop();
+            int weight2 = weights.top();
+            weights.pop();
+
+            weights.push(abs(weight1 - weight2));
         }
 
-        int stone1,stone2;
-
-        while(!pack.empty())
-        {
-            stone1 = pack.top();
-            pack.pop();
-
-            if(!pack.empty())
-            {
-                stone2 = pack.top();
-                pack.pop();
-
-                int smash = abs(stone1 - stone2);
-                pack.push(smash);   
-            }
-        }
-
-        return stone1;
+        return weights.top();
     }
 };
