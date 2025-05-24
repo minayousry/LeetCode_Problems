a4aa13c2b8fdab79260e81a8a3790e4a805bd798
Mina Samy <mina.yousry.iti@gmail.com>
2025-05-24 01:36:45 +0100

    Time: 31 ms (6.47%), Space: 23.1 MB (56.72%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -11,26 +11,30 @@
  */
 class Solution {
 public:
-
-    pair<bool,int> checkbalanced(TreeNode* root)
+    pair<bool,int> isSubtreeBalanced(TreeNode* root)
     {
-        bool result;
-        if(!root)
+        if(root == nullptr)
         {
             return {true,0};
         }
         else
         {
-            auto left = checkbalanced(root->left);
-            auto right = checkbalanced(root->right);
+            auto left_branch = isSubtreeBalanced(root->left);
+            auto right_branch = isSubtreeBalanced(root->right);
 
-            int diff = abs(left.second - right.second);
-            result = left.first && right.first && (diff < 2);
-            return {result,1 + max(left.second,right.second)};
+            bool result = left_branch.first && right_branch.first && (abs(left_branch.second - right_branch.second) <= 1 );
+            
+            cout<<1 + max(left_branch.second,right_branch.second)<<endl;
+            return {result,1 + max(left_branch.second,right_branch.second)};
+            
         }
     }
 
     bool isBalanced(TreeNode* root) {
-        return checkbalanced(root).first;
-;    }
+        if(root)
+        {
+            return isSubtreeBalanced(root).first;
+        }
+        return true;
+    }
 };
