fa5f54b36e9d6b21841d57dd415f2c6d84bfa080
Mina Samy <mina.yousry.iti@gmail.com>
2025-05-24 17:32:59 +0100

    Time: 0 ms (100%), Space: 21.8 MB (76.07%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -9,77 +9,31 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
 class Solution {
 public:
 
-
-    bool check_left(TreeNode* root,int node_val)
+    bool checkValidity(TreeNode* root,long left,long right)
     {
-        if(!root)
+        if(root == nullptr)
         {
             return true;
         }
-        else if(root->val >= node_val)
+        else if(left >= root->val || right <= root->val)
         {
             return false;
         }
         else
         {
-            return check_left(root->left,node_val) && check_left(root->right,node_val);
-        }
-    }
+            return checkValidity(root->left,left,root->val) && checkValidity(root->right,root->val,right);
 
-    bool check_right(TreeNode* root,int node_val)
-    {
-        if(!root)
-        {
-            return true;
         }
-        else if(root->val <= node_val)
-        {
-            return false;
-        }
-        else
-        {
-            return check_right(root->left,node_val) && check_right(root->right,node_val);
-        }
-    }
 
+    }
 
     bool isValidBST(TreeNode* root) {
-
-        bool result = true;
-
-        queue<tuple<TreeNode*,long,long>> data;
-
-        if(root)
-        {
-            data.push(make_tuple(root,LONG_MIN,LONG_MAX));
-        }
         
-        while(!data.empty())
-        {
-            auto [elm,left_limit,right_limit] = data.front();
-            data.pop();
-
-            if((left_limit >= elm->val) || (right_limit <= elm->val))
-            {
-                return false;
-            } 
-
-            if(elm->left)
-            {
-                data.push(make_tuple(elm->left,left_limit,elm->val));
-            }
-
-            if(elm->right)
-            {
-                data.push(make_tuple(elm->right,elm->val,right_limit));
-            }
-        }
-        return true;
-        
-        
+          return checkValidity(root,LONG_MIN,LONG_MAX);
     }
+
+
 };
